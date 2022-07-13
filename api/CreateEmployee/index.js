536e49a11c0c18ac6428396d47f236be5e656fe3
Torr7s <torresmiranda.rj@gmail.com>
2022-07-13 15:30:56 -0300

    feat(api): create new azure function CreateEmployee

---
+++ api/CreateEmployee/index.js
@@ -0,0 +1,42 @@
+const prisma = require('../shared/prisma/prisma.client');
+const errorHandler = require('../shared/errors');
+
+module.exports = async function (context, req) {
+  try {
+    const {
+      name,
+      salary,
+      job_role,
+      employee_registration
+    } = req.body;
+  
+    employee_registration = parseInt(employee_registration);
+  
+    const employeeRegistrationExists = await prisma.employee.findUnique({
+      where: {
+        employee_registration
+      }
+    });
+  
+    if (employeeRegistrationExists) return errorHandler(490, 'Employee already exists.');
+  
+    const employee = await prisma.employee.create({
+      data: {
+        name,
+        salary,
+        job_role,
+        employee_registration
+      }
+    });
+  
+    return {
+      status: 201,
+      body: employee
+    }
+  } catch (error) {
+    context.log('Error while trying to create an employee.');
+    context.error(error);
+
+    return errorHandler(500, error);
+  }
+}
